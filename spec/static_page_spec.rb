41ebd579fb8af0ea286013623062c1638dacfef7
Alekseii Smirnov <alekseii.sm@gmail.com>
2021-06-08 03:09:29 +0300

    adds static prototype

--- spec/static_page_spec.rb
+++ spec/static_page_spec.rb
@@ -7,9 +7,28 @@ set(:show_exceptions, false)
 
 describe('Static prototype', type: :feature) do
   context 'Enterance' do
+    before :each do
+      visit 'static/1'
+    end
     it 'is a page describing enterance' do
-      visit('static/1')
       expect(page).to have_content('Enterance here')
+    end
+    it 'has a link `Door #1` to the Hall' do
+      click_on 'Door #1'
+      expect(page).to have_content('The Main Hall')
+    end
+  end
+
+  context 'Walkthrough' do
+    it 'is possible to go to `Dead End` and then to the exit' do
+      path = ['Door #1', 'Door #1', 'Door #0', 'Door #2', 'Door #1']
+
+      visit 'static/1'
+      path.each do |door|
+        click_on door
+      end
+
+      expect(page).to have_content('EXIT') 
     end
   end
 end
