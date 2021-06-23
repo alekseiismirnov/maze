18e36c44666cac0118de07a9bce4272add573e59
Alekseii Smirnov <alekseii.sm@gmail.com>
2021-06-23 20:09:00 +0300

    add previous page link test ok

--- spec/room_create_integration_spec.rb
+++ spec/room_create_integration_spec.rb
@@ -12,15 +12,17 @@ describe('Room#create', type: :feature) do
       visit "/rooms/#{@enterance.id}"
 
       click_on 'Digg Next Room'
-
-      save_and_open_page
-
       fill_in 'description',	with: @description
       click_on 'Finish!'
     end
 
     it 'redirect to new room page' do
       expect(page).to have_content @description
+    end
+
+    it 'has a link on the ancesstor`s page' do
+      click_on "Door ##{@enterance.id}"
+      expect(page).to have_content @enterance.description
     end
   end
 end
