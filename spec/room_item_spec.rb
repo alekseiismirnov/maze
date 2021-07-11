e43a72d5972ae8dff7008f2e4811d80ed7966766
Alekseii Smirnov <alekseii.sm@gmail.com>
2021-07-12 01:10:59 +0300

    reorganize room_item_cpec

--- spec/room_item_spec.rb
+++ spec/room_item_spec.rb
@@ -4,22 +4,28 @@ require 'room'
 require 'item'
 
 describe Room do
-  context 'Room#all_items' do
-    before :each do
-      @room = Room.new('Some room', [1, 2], Room.free_id)
-    end
-    it 'returns [] if there is no items in the room' do
+  before :each do
+    @room = Room.new('Some room', [1, 2], Room.free_id)
+  end
+
+  context 'No items in the room' do
+    it 'Room#all_items returns [] if there is no items in the room' do
       expect(@room.all_items).to eq []
     end
+  end
 
-    it 'returns a list of items' do
-      items = [
+  context 'If there are some items in the room' do
+    before :each do
+      @items = [
         Item.new(type: 'something', detail: 'have no idea what it is').save,
         Item.new(type: 'whatever', detail: 'something different').save
       ]
-      @room.put_item items[0]
-      @room.put_item items[1]
-      expect(@room.all_items).to eq items
+      @room.put_item @items[0]
+      @room.put_item @items[1]
+    end
+
+    it 'Room#all_returns a list of items' do
+      expect(@room.all_items).to eq @items
     end
   end
 end
