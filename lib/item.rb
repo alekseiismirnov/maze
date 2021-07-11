c6b969270c29b59ea86a038c12b3a67c3c5e0b5f
Alekseii Smirnov <alekseii.sm@gmail.com>
2021-07-11 14:00:12 +0300

    add saving items

--- lib/item.rb
+++ lib/item.rb
@@ -2,10 +2,39 @@
 
 # Items, could be founded in the maze
 class Item
+  attr_reader :id, :type, :detail
+
+  @items = {}
+  @total_rows = 0
+
+  def initialize(params)
+    @type = params[:type]
+    @detail = params[:detail]
+    @id = params[:id]
+  end
+
   def self.clear
+    @items = {}
   end
 
   def self.all
-    []
+    @items.values
+  end
+
+  def self.free_id
+    @total_rows += 1
+  end
+
+  def self.add_item(item)
+    @items[item.id] = item
+  end
+
+  def ==(other)
+    @type == other.type && @detail == other.detail
+  end
+
+  def save
+    id = self.class.free_id
+    self.class.add_item Item.new(type: @type, detail: @detail, id: id)
   end
 end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,10 +1,25 @@
+# frozen_string_literal: true
+
 require 'item'
 
 describe Item do
+  before :each do
+    @catalog = (1..10).to_a.map do |i|
+      { type: "Item ##{i}", detail: "Description of the item ##{i}" }
+    end
+
+    @items = @catalog.map { |record| Item.new(record) }
+    @items.each(&:save)
+  end
+
   context 'Item#all' do
     it 'return [] if there is no objects' do
       Item.clear
       expect(Item.all).to eq []
+    end
+
+    it '#all returns all saved items' do
+      expect(Item.all).to eq @items # should we sort?
     end
   end
 end
